377c3db004e9ac50ff1a2567f96f3a05e38b2a81
meitii1212 <meitalgl@post.bgu.ac.il>
2022-05-09 19:49:11 +0300

    finish classes

--- src/Domain/Referee.java
+++ src/Domain/Referee.java
@@ -1,8 +1,56 @@
 package Domain;
 
+import java.util.ArrayList;
+
 public class Referee extends SignedUser {
+
+    String refNum;
+    League league;
+    String qualification;
+    ArrayList<Game> games_list;
+
+    public String getRefNum() {
+        return refNum;
+    }
+
+    public void setRefNum(String refNum) {
+        this.refNum = refNum;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setLeague(League league) {
+        this.league = league;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public ArrayList<Game> getGames_list() {
+        return games_list;
+    }
+
+    public void setGames_list(ArrayList<Game> games_list) {
+        this.games_list = games_list;
+    }
 
     public Referee(String userName, String password) {
         super(userName, password);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Referee{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
--- src/Domain/UnionRep.java
+++ src/Domain/UnionRep.java
@@ -1,4 +1,12 @@
 package Domain;
 
-public class UnionRep {
+public class UnionRep extends SignedUser {
+
+    String UnionNum;
+
+    public UnionRep(String userName, String password) {
+        super(userName, password);
+    }
+
+
 }
